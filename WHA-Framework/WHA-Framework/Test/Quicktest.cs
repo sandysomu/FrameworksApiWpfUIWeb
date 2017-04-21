3c22c33544261285b7ff58df6cde6e0a2f7d4c90
PROD\sandeep.singh <sandeep.singh@williamhill.com.au>
2017-04-21 15:56:59 +1000

    vanila- nuni installed and folder structure defined

---
+++ WHA-Framework/WHA-Framework/Test/Quicktest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+//using WHA.DBUtilities;
+
+
+namespace WHA.Test
+{
+    [TestFixture]
+    class Quicktest
+    {
+
+
+        [Test]
+        public void Test()
+        {
+
+            //using (var db = new WHAprojectDBEntities())
+            //{
+            //    db.temps.Add(new temp() {FirstName = "Haren", Id = 3, SurName = "Singh"});
+            //    db.Dispose();
+
+            //}
+
+            //List<string> data = new List<string>();
+
+            
+        }
+
+    }
+}
