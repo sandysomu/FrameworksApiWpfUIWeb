2e4f69a21d9ae00271e4bcbe70905073cc9377fa
PROD\sandeep.singh <sandeep.singh@williamhill.com.au>
2017-05-01 19:48:12 +1000

    added sqlite to service project

---
+++ WHA-Framework/WHA.Framework.WebApi/Test/Testing.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using WHA_Framework.Services;
+
+namespace WHA.Framework.WebApi.Test
+{
+    [TestFixture]
+    public class Testing
+    {
+        private ITransactionService tran;
+
+        [Test]
+        public void test1Sandy()
+        {
+            tran = new TransactionService();
+            tran.GetTransaction(3);
+        }
+    }
+}
