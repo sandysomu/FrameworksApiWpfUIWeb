7ef0dac89ad2e36c5dd3f8205f51d03730390b6d
Sandeep Singh <sandysiomu@gmail.com>
2018-10-30 15:52:24 +1100

    New Update

--- wha-framework/WHA-Framework/WHA.Framework.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WHA.Framework.WebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- wha-framework/WHA-Framework/WHA.Framework.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WHA.Framework.WebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
